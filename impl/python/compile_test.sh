2cad86f90eb62ec53aabe37c51dde83039e758f4
pegasust <pegasucksgg@gmail.com>
2022-11-04 02:02:48 -0700

    offsets: test input actually gives bad output

--- impl/python/compile_test.sh
+++ impl/python/compile_test.sh
@@ -1,5 +1,18 @@
 #!/usr/bin/env sh
-ls ../../examples/omega | grep -o .*\.omega | while read input; do  
-    echo "Input: ${input}"
-    python3 main.py --file ../../examples/omega/${input}
+SUBDIR=${1:-"../../examples/omega"}
+touch test_output/compile_test.out
+touch test_output/compile_test.err
+
+echo "=====================">> test_output/compile_test.out
+echo "=====================">> test_output/compile_test.err
+
+date >> test_output/compile_test.out
+date >> test_output/compile_test.err
+
+find "${SUBDIR}" | grep -o ".*\.omega" | while read input; do  
+    echo "Input: ${input}, output: ${input}.vm" |\
+        tee test_output/compile_test.out | tee test_output/compile_test.err
+    python3 main.py --file ${input} --output ${input}.vm --run \
+        >>test_output/compile_test.out 2>>test_output/compile_test.err
+    echo "Exit: $?"
 done
--- impl/python/main.py
+++ impl/python/main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+from sys import stdout
 from typing import List
 
 from asts import Program
@@ -11,17 +12,30 @@ import codegen
 import offsets
 from vm_insns import Insn
 import vm_utils
+import vm_insns
 
 
 def main():
     args = get_args()
     fname = args.file
+    compiled_source = None
     with open(fname) as f:
         input = f.read()
-    interpret(input, args.args, args.verbose)
+        compiled_source = compile(input)
+
+    if not compiled_source:
+        raise RuntimeError(f"Compiling {fname} yields None for some reason")
+
+    with open(args.output, "w") as f:
+        stdout.writelines(
+            (vm_insns.dis(isns)+"\n" for isns in compiled_source))
+        f.writelines((vm_insns.dis(isns)+"\n" for isns in compiled_source))
+
+    if args.run:
+        interpret(compiled_source, args.args, args.verbose)
 
 
-def interpret(input, args, verbose):
+def compile(input):
     lexer = Scanner(input)
     psr = Parser(lexer)
     tree: Program = psr.parse()
@@ -29,6 +43,10 @@ def interpret(input, args, verbose):
     typecheck.program(tree)
     offsets.program(tree)
     insns: List[Insn] = codegen.generate(tree)
+    return insns
+
+
+def interpret(insns: list[Insn], args, verbose):
     vm_utils.invoke_omega(insns, args, verbose)
 
 
@@ -37,6 +55,7 @@ def get_args():
         description="Compile Omega files"
     )
     ap.add_argument("--file", help="The file to compile")
+    ap.add_argument("--output", help="Output filename", default="a.omega")
     ap.add_argument(
         "--verbose",
         action="store_true",
@@ -45,6 +64,8 @@ def get_args():
     ap.add_argument(
         "args", nargs="*", help="Arguments to pass to the program as integers"
     )
+    ap.add_argument("--run", action="store_true",
+                    help="Run the program after compilation")
     return ap.parse_args()
 
 
